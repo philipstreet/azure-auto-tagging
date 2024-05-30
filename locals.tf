e59e4311fe479917b47f5fbf8f13f8eed322b1ba
Philip Street <philip.street@kainos.com>
2024-05-30 18:20:55 +0100

    chore: remove some tags

--- locals.tf
+++ locals.tf
@@ -2,9 +2,6 @@ locals {
 
   # Tags to be applied to all resources
   tags = {
-    "Chargeback"  = "CTS"
-    "Company"     = "CTS"
-    "LOB"         = "CTS"
     "Environment" = title(var.environment)
   }
 }
