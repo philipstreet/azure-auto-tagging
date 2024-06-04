aa3fdc593c44f6d66e41eaab349db1bb039ca6fb
Philip Street <philip.street@kainos.com>
2024-06-04 14:54:47 +0100

    chore: Update Azure pipeline YAML files to use script file

---
+++ scripts/yor.sh
@@ -0,0 +1,12 @@
+# download Yor
+curl -s -k https://api.github.com/repos/bridgecrewio/yor/releases/latest | jq '.assets[] | select(.name | contains("linux_386")) | select(.content_type | contains("gzip")) | .browser_download_url' -r | awk '{print "curl -L -k " $0 " -o yor.tar.gz"}' | sh
+tar -xf yor.tar.gz
+chmod +x yor
+./yor --version
+
+# run Yor
+yor tag --directory tagging --dry-run --parsers Terraform
+yor tag --directory tagging --parsers Terraform
+
+# show folder contents
+ls -al
