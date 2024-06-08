503d883d2a133052f55cb9f02ba0aaab64bead08
Philip Street <philip.street@kainos.com>
2024-06-08 14:02:08 +0100

    chore: Add script to retrieve git metadata

---
+++ scripts/get-git-metadata.sh
@@ -0,0 +1,12 @@
+# get commit user name
+GIT_LAST_COMMIT_USER_NAME=$(git show -s --format=%an 2>&1)
+# get commit user email
+GIT_LAST_COMMIT_USER_EMAIL=$(git show -s --format=%ae 2>&1)
+# get commit datetime
+GIT_LAST_COMMIT_DATE=$(git show -s --format=%ci 2>&1)
+# get repo url
+GIT_REPO_URL=$(git ls-remote --get-url 2>&1)
+# get branch name
+GIT_BRANCH=${GITHUB_REF#refs/heads/}
+# set TAGS variable
+export TAGS=$(echo "{\"git_branch_name\":\"$GIT_BRANCH\",\"git_last_commit_by_name\":\"$GIT_LAST_COMMIT_USER_NAME\",\"git_last_commit_by_email\":\"$GIT_LAST_COMMIT_USER_EMAIL\",\"git_last_commit_at\":\"$GIT_LAST_COMMIT_DATE\",\"git_repo_url\":\"$GIT_REPO_URL\",\"git_commit\":\"$GITHUB_SHA\"}" | sed 's/ /_/g')
