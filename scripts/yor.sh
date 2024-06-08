c2f1e45ab0427967f31e4e7accf10e420d3108e3
Philip Street <philip.street@kainos.com>
2024-06-08 10:57:16 +0100

    chore: Update Yor installation and usage in Terratag script

--- scripts/yor.sh
+++ scripts/yor.sh
@@ -1,12 +1,15 @@
 # download Yor
+echo "Downloading Yor"
 curl -s -k https://api.github.com/repos/bridgecrewio/yor/releases/latest | jq '.assets[] | select(.name | contains("linux_386")) | select(.content_type | contains("gzip")) | .browser_download_url' -r | awk '{print "curl -L -k " $0 " -o yor.tar.gz"}' | sh
+echo "Extracting Yor"
 tar -xf yor.tar.gz
+echo "Set execute permissions"
 chmod +x yor
+echo "Display Yor version"
 ./yor --version
+echo "Show folder contents"
+ls -al
 
 # run Yor
-yor tag --directory tagging --dry-run --parsers Terraform
-yor tag --directory tagging --parsers Terraform
-
-# show folder contents
-# ls -al
+./yor tag --directory tagging --dry-run --parsers Terraform
+./yor tag --directory tagging --parsers Terraform
