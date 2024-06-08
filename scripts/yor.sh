507032e94b0fbec3a6fbf9465e5ee94e2e7e0717
Philip Street <philip.street@kainos.com>
2024-06-08 11:06:35 +0100

    chore: Tweak echos

--- scripts/yor.sh
+++ scripts/yor.sh
@@ -1,13 +1,13 @@
 # download Yor
-echo "Downloading Yor"
+echo "** Downloading Yor **"
 curl -s -k https://api.github.com/repos/bridgecrewio/yor/releases/latest | jq '.assets[] | select(.name | contains("linux_386")) | select(.content_type | contains("gzip")) | .browser_download_url' -r | awk '{print "curl -L -k " $0 " -o yor.tar.gz"}' | sh
-echo "Extracting Yor"
+echo "** Extracting Yor **"
 tar -xf yor.tar.gz
-echo "Set execute permissions"
+echo "** Set execute permissions **"
 chmod +x yor
-echo "Display Yor version"
+echo "** Display Yor version **"
 ./yor --version
-echo "Show folder contents"
+echo "** Show folder contents **"
 ls -al
 
 # run Yor
