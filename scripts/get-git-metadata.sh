923b4655cd4846ce08f44f257f5a4de6bc2a2af9
Philip Street <philip.street@kainos.com>
2024-06-08 15:09:48 +0100

    chore: Update git metadata retrieval script

--- scripts/get-git-metadata.sh
+++ scripts/get-git-metadata.sh
@@ -8,5 +8,8 @@ GIT_LAST_COMMIT_DATE=$(git show -s --format=%ci 2>&1)
 GIT_REPO_URL=$(git ls-remote --get-url 2>&1)
 # get branch name
 GIT_BRANCH=${GITHUB_REF#refs/heads/}
+#get git commit sha
+GIT_COMMIT_SHA=$(git rev-parse HEAD)
+
 # set TAGS variable
-export TAGS=$(echo "{\"git_branch_name\":\"$GIT_BRANCH\",\"git_last_commit_by_name\":\"$GIT_LAST_COMMIT_USER_NAME\",\"git_last_commit_by_email\":\"$GIT_LAST_COMMIT_USER_EMAIL\",\"git_last_commit_at\":\"$GIT_LAST_COMMIT_DATE\",\"git_repo_url\":\"$GIT_REPO_URL\",\"git_commit\":\"$GITHUB_SHA\"}" | sed 's/ /_/g')
+export TAGS=$(echo "{\"git_branch_name\":\"$GIT_BRANCH\",\"git_last_commit_by_name\":\"$GIT_LAST_COMMIT_USER_NAME\",\"git_last_commit_by_email\":\"$GIT_LAST_COMMIT_USER_EMAIL\",\"git_last_commit_at\":\"$GIT_LAST_COMMIT_DATE\",\"git_repo_url\":\"$GIT_REPO_URL\",\"git_commit\":\"$GIT_COMMIT_SHA\"}" | sed 's/ /_/g')
