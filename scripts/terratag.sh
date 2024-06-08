76dddc8d6e9fa6a13c084a1cbcadd6d6f024f772
Philip Street <philip.street@kainos.com>
2024-06-08 13:19:41 +0100

    chore: use same tag code as GH pipeline

--- scripts/terratag.sh
+++ scripts/terratag.sh
@@ -22,23 +22,38 @@ curl -O -L $tar_url
 tar -xvf $tar 
 rm -rf $tar
 
+# # get commit user name
+# export last_commit_user_name=$(git show -s --format=%an 2>&1)
+# # get commit user email
+# export last_commit_user_email=$(git show -s --format=%ae 2>&1)
+# # get commit datetime
+# export last_commit_datetime=$(git show -s --format=%ci 2>&1)
+# # get repo url
+# export repo_url=$(git ls-remote --get-url 2>&1)
+# # get commit hash
+# export commit_hash=$(git rev-parse HEAD 2>&1)
+# # get branch name
+# export branch_name=$(git rev-parse --abbrev-ref HEAD 2>&1)
+# # create tags
+# export tags=$(echo "{\"git_branch_name\":\"$branch_name\",\"git_user_name\":\"$last_commit_user_name\",\"git_user_email\":\"$last_commit_user_email\",\"git_commit_datetime\":\"$last_commit_datetime\",\"git_repo_url\":\"$repo_url\",\"git_commit_hash\":\"$commit_hash\"}" | sed 's/ /_/g')
+
 # get commit user name
-export last_commit_user_name=$(git show -s --format=%an 2>&1)
+GIT_LAST_COMMIT_USER_NAME=$(git show -s --format=%an 2>&1)
 # get commit user email
-export last_commit_user_email=$(git show -s --format=%ae 2>&1)
+GIT_LAST_COMMIT_USER_EMAIL=$(git show -s --format=%ae 2>&1)
 # get commit datetime
-export last_commit_datetime=$(git show -s --format=%ci 2>&1)
+GIT_LAST_COMMIT_DATE=$(git show -s --format=%ci 2>&1)
 # get repo url
-export repo_url=$(git ls-remote --get-url 2>&1)
+GIT_REPO_URL=$(git ls-remote --get-url 2>&1)
 # get commit hash
-export commit_hash=$(git rev-parse HEAD 2>&1)
+GIT_COMMIT_SHA=${GITHUB_SHA}
 # get branch name
-export branch_name=$(git rev-parse --abbrev-ref HEAD 2>&1)
-# create tags
-export tags=$(echo "{\"git_branch_name\":\"$branch_name\",\"git_user_name\":\"$last_commit_user_name\",\"git_user_email\":\"$last_commit_user_email\",\"git_commit_datetime\":\"$last_commit_datetime\",\"git_repo_url\":\"$repo_url\",\"git_commit_hash\":\"$commit_hash\"}" | sed 's/ /_/g')
+GIT_BRANCH=${GITHUB_REF#refs/heads/}
+# set TAGS variable
+TAGS=$(echo "{\"user_name\":\"$GIT_LAST_COMMIT_USER_NAME\",\"user_email\":\"$GIT_LAST_COMMIT_USER_EMAIL\",\"commit_datetime\":\"$GIT_LAST_COMMIT_DATE\",\"repo_url\":\"$GIT_REPO_URL\",\"commit_hash\":\"$GIT_COMMIT_SHA\"}" | sed 's/ /_/g')
 
 # apply Terratag tags
-./terratag -dir=./ -tags=$tags
+./terratag -dir=./ -tags=$TAGS
 
 # show folder contents
 # ls -al
