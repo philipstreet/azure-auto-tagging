eb9bb89e91164bdc87829051da736a6b307f5911
Philip Street <philip.street@kainos.com>
2024-06-09 13:50:49 +0100

    chore: Split resources into separate files

--- main.tf
+++ main.tf
@@ -9,37 +9,3 @@ resource "azurerm_resource_group" "this" {
   name     = module.naming.resource_group.name_unique
   tags     = local.tags
 }
-
-resource "azurerm_storage_account" "this" {
-  account_replication_type      = "LRS"
-  account_tier                  = "Standard"
-  name                          = "${module.naming.storage_account.name_unique}001"
-  location                      = azurerm_resource_group.this.location
-  resource_group_name           = azurerm_resource_group.this.name
-  enable_https_traffic_only     = true
-  public_network_access_enabled = true
-  min_tls_version               = "TLS1_2"
-  tags                          = local.tags
-}
-
-module "local_storage_account" {
-  source              = "./modules/test"
-  name                = "${module.naming.storage_account.name_unique}002"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  tags                = local.tags
-}
-
-module "remote_storage_account" {
-  source                        = "Azure/avm-res-storage-storageaccount/azurerm"
-  version                       = ">= 0.1.1"
-  name                          = "${module.naming.storage_account.name_unique}003"
-  resource_group_name           = azurerm_resource_group.this.name
-  location                      = azurerm_resource_group.this.location
-  enable_https_traffic_only     = true
-  public_network_access_enabled = true
-  shared_access_key_enabled     = true
-  account_replication_type      = "LRS"
-  network_rules                 = null
-  tags                          = local.tags
-}
