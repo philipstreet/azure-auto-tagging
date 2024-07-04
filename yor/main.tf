4c1c52a734faf6f46eca6a47601253ca52075799
Philip Street <philip.street@kainos.com>
2024-07-04 09:59:07 +0100

    chore: Update yor.sh script path and remove unnecessary tag group option

--- yor/main.tf
+++ yor/main.tf
@@ -7,15 +7,5 @@ module "naming" {
 resource "azurerm_resource_group" "this" {
   location = var.location
   name     = module.naming.resource_group.name_unique
-  tags = merge(local.tags, {
-    git_commit           = "239ce6646943cea5feea5e9230d1b8a61737bc4c"
-    git_file             = "yor/main.tf"
-    git_last_modified_at = "2024-06-22 08:00:07"
-    git_last_modified_by = "philip.street@kainos.com"
-    git_modifiers        = "philip.street"
-    git_org              = "philipstreet"
-    git_repo             = "azure-auto-tagging"
-    yor_name             = "this"
-    yor_trace            = "9375282e-c2d4-4cf3-b248-d7ae0720a7f5"
-  })
+  tags     = local.tags
 }
--- yor/storage_remote_module.tf
+++ yor/storage_remote_module.tf
@@ -10,15 +10,5 @@ module "remote_storage_account" {
   #cross_tenant_replication_enabled = false
   account_replication_type = "LRS"
   network_rules            = null
-  tags = merge(local.tags, {
-    git_commit           = "2e55fa8a14a01dc76f5dfad9de9331eb0ef6e86b"
-    git_file             = "yor/storage_remote_module.tf"
-    git_last_modified_at = "2024-06-20 16:54:17"
-    git_last_modified_by = "philip.street@kainos.com"
-    git_modifiers        = "philip.street"
-    git_org              = "philipstreet"
-    git_repo             = "azure-auto-tagging"
-    yor_name             = "remote_storage_account"
-    yor_trace            = "fa3c0b01-8a70-448d-a864-3474551c20fd"
-  })
+  tags                     = local.tags
 }
